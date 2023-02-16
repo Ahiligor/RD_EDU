2554e2fa8a266e04795aebd2e0084816b507f1a3
Igor Gavrylishyn <ahiligor@gmail.com>
2023-02-16 22:46:22 +0100

    Home Work in progress

--- Lection_4/HW_4.py
+++ Lection_4/HW_4.py
@@ -1,3 +1,18 @@
-a = 1
-b = 2
-# TODO: Need add some logic here
+"""
+1. Створити програму, яка буде очікувати введення тексту від користувача і повідомляти —
+чи є введений текст “числом” чи “словом”.
+
+2. Якщо введений текст “число”, програма має також вказати чи є воно парним чи непарним.
+
+3. Якщо це “слово”, програма має вказати його довжину.
+"""
+
+text_or_value = input("Enter please any text ")
+
+if text_or_value is int:
+    print("This is 'number")
+else:
+    print("This is text")
+
+print(type(text_or_value))
+print(text_or_value)
