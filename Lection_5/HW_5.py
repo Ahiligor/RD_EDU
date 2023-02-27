1973581f27e7c29a6dff775ae6cd4c1a12250e61
Igor Gavrylishyn <ahiligor@gmail.com>
2023-02-28 00:28:28 +0100

    HW_5 in progress

--- Lection_5/HW_5.py
+++ Lection_5/HW_5.py
@@ -1,1 +1,25 @@
-print("Hello World")
+"""
+Task 1. 1. Створити програму, яка буде очікувати від користувача введення тексту
+і виведе інформацію по кожному надрукованому символу:
+
+це “число” + яке воно (парне, непарне),
+це “буква” + яка вона (велика чи маленька),
+це “символ”
+"""
+
+text = input("Enter any text please: ")
+
+for letter in text:
+    if letter.isdigit():
+        if int(letter) % 2 == 0:
+            print("This is a digit and it is even")
+        else:
+            print("This is a digit and it is odd")
+    elif letter.isupper():
+        print("Letter is Capital")
+    elif letter.islower():
+        print("Letter is Small")
+    elif letter.isspace():
+        print("This is space")
+    elif letter.isalnum():
+        print("This is a symbol")
